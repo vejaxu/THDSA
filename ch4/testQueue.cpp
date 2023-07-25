32e426ca7487dd047d6b442a1c83dcb2570084aa
wjxu29 <wjxu29@gmail.com>
2023-07-25 15:31:46 +0800

    Update testQueue.cpp

--- ch4/testQueue.cpp
+++ ch4/testQueue.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
 #include "MyQueue.h"
+#include "../Visit.h"
 
 
-template <typename T>
-void traverseQ(T e){
-    std::cout << e << ' ';
-}
 template <typename T>
 void output(Queue<T> Q){
-    std::cout << "current Queue size: " << Q.size() << std::endl;
-    std::cout << "current Queue content: " << std::endl;
-    Q.traverse(traverseQ);
+    Visit visit;
+    Q.traverse(visit);
     std::cout << std::endl;
 }
 
 
-main(){
+int main(){
     Queue<int> myQueue = Queue<int>();
     for (int i = 0; i < 10; i ++){
         myQueue.enqueue(i);
     }
     output(myQueue);
-    std::cout << "队首出队: " << std::endl;
-    std::cout << "出队元素为: " << myQueue.dequeue() << std::endl;
-    output(myQueue);
-    std::cout << "队尾入队" << std::endl;
     myQueue.enqueue(10);
     output(myQueue);
+    myQueue.dequeue();
+    output(myQueue);
+    std::cout << myQueue.front() << std::endl;
 }
