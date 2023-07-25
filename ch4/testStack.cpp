390f1963c4cd2442a5f45c4c9d27687a61b0af27
wjxu29 <wjxu29@gmail.com>
2023-07-25 15:02:45 +0800

    Update testStack.cpp

--- ch4/testStack.cpp
+++ ch4/testStack.cpp
@@ -1,39 +1,26 @@
 #include <iostream>
 #include "MyStack.h"
-using namespace std;
-
-
-//遍历输出
-template <typename T>
-void traverseV(T e){
-    std::cout << e << ' ';
-}
+#include "../Visit.h"
 
 
 template <typename T>
-void output(Stack<T> V){
-    std::cout << "current Stack size: " << V.size() << std::endl;
-    std::cout << "current Stack capacity: " << V.capacity() << std::endl;
-    std::cout << "current Stack content: " << std::endl;
-    V.traverse(traverseV);
+void output(Stack<T> stack){
+    Visit visit;
+    stack.traverse(visit);
     std::cout << std::endl;
 }
 
 
 int main(){
+        Visit visit;
     Stack<int> myStack = Stack<int>();
     for (Rank i = 0; i < 10; i ++)
         myStack.push(i);
     output(myStack);
-    cout << "删除栈顶元素为: " << myStack.pop() << endl;
+    std::cout << myStack.pop();
+    std::cout << std::endl;
     output(myStack);
-    cout << "删除栈顶元素: " << myStack.top() << endl;
-
-    Stack<int> reverseStack = Stack<int>();
-    int mystack_size = myStack.size();
-    for (Rank i = 0; i < mystack_size; i ++){
-        reverseStack.push(myStack.pop());
-    }
-    cout << "栈的逆序: " << endl;
-    output(reverseStack);
+    myStack.push(9);
+    output(myStack);
+    std::cout << myStack.top() << std::endl;
 }
