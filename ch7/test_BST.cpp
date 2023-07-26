7557e3d99b5002c4dd76d869a7b972c20fa4908a
wjxu29 <wjxu29@gmail.com>
2023-07-26 17:11:03 +0800

    Update test_BST.cpp

--- ch7/test_BST.cpp
+++ ch7/test_BST.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include "BST.h"
-
-class Visit{
-    public:
-        template<typename T>
-        void operator()(T value){
-            std::cout << value << "  ";
-        }
-};
+#include "../Visit.h"
 
 
 int main(){
     Visit visit;
     BST<int> myBst = BST<int>();
+    myBst.insertAsRoot(10);
+    myBst.travPre_I1(myBst.root(), visit);
+    std::cout << std::endl;
+    myBst.insert(20);
+    myBst.travPre_I1(myBst.root(), visit);
+    std::cout << std::endl;
+    myBst.insert(5);
+    myBst.travPre_I1(myBst.root(), visit);
+    std::cout << std::endl;
+    myBst.remove(20);
+    myBst.travPre_I1(myBst.root(), visit);
+    std::cout << std::endl;
 }
