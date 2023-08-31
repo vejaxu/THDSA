951950878df46b523254c1fe9bdf47ac5e9eb926
wjxu29 <wjxu29@gmail.com>
2023-08-31 23:20:32 +0800

    Create KMP.cpp

---
+++ ch13/KMP.cpp
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <iostream>
+
+
+int* buildNext(char* P){
+    size_t m = strlen(P), j = 0;
+    int* N = new int[m];
+    int t = N[0] = - 1;
+    while(j < m - 1){
+        if(0 > t || P[j] == P[t]){
+            j ++;
+            t ++;
+            N[j] = t;
+        }else{
+            t = N[t];
+        }
+    }
+    return N;
+}
+int match(char* P, char* T){
+    int* next = buildNext(P);
+    int n = (int) strlen(T), i = 0;
+    int m = (int) strlen(P), j = 0;
+
+    while(i < n && j < m){
+        if(0 > j || T[i] == P[j]){
+            i ++;
+            j ++;
+        }else{
+            j = next[j];
+        }
+    }
+
+    delete [] next;
+
+    return (i - j);
+}
+
+
+int main(){
+    char T[] = {'a', 'a', 'd', 'a', 'a', 'b', 'a', 'a', 'f'};
+    char P[] = {'a', 'a', 'b', 'a', 'a', 'f'};
+
+    int result = match(P, T);
+
+    std::cout << result << std::endl;
+
+    return result;
+
+}
